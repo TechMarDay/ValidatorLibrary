85781a535edeadf8314e97863f9863430eb79d3c
LongNguyendh <longdhn.lk@gmail.com>
2021-07-01 22:08:37 +0700

    validator for checkbox, radio and file

--- validator.js
+++ validator.js
@@ -1,12 +1,36 @@
 function Validator(options){
+
+    function getParent(element, selector){
+        while(element.parentElement){
+            if(element.parentElement.matches(selector)){
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    }
+
+    var selectorRules = {};
+    var formElement = document.querySelector(options.form);
+
     function validate(inputElement, rule){
         var errorMessage;
-        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+        var parrentElement = getParent(inputElement, options.formGroupSelector);
+        var errorElement = parrentElement.querySelector(options.errorSelector);
 
         var rules = selectorRules[rule.selector];
         for(var i = 0; i < rules.length; i++)
         {
-            errorMessage = rules[i](inputElement.value);
+            switch(inputElement.type){
+                case 'radio':
+                case 'checkbox':
+                    errorMessage = rules[i](
+                        formElement.querySelector(rule.selector + ':checked')
+                    );
+                    break;
+                default:
+                    errorMessage = rules[i](inputElement.value);
+            }
+
             if(errorMessage)
                 break;
         }
@@ -14,11 +38,11 @@ function Validator(options){
         if(errorMessage)
         {
             errorElement.innerText = errorMessage;
-            inputElement.parentElement.classList.add('invalid');
+            parrentElement.classList.add('invalid');
         }
         else{
             errorElement.innerText = '';
-            inputElement.parentElement.classList.remove('invalid');
+            parrentElement.classList.remove('invalid');
         }
 
         return !errorMessage;
@@ -26,9 +50,6 @@ function Validator(options){
 
     }
 
-    var selectorRules = {};
-
-    var formElement = document.querySelector(options.form);
     if(formElement)
     {
         formElement.onsubmit = function(e){
@@ -48,11 +69,31 @@ function Validator(options){
             if(isFormValid){
                 //Trường hợp submit với Javascript
                 if(typeof options.onSubmit === 'function'){
+                    debugger;
                     var enableInputs = formElement.querySelectorAll('[name]:not([disabled])');
                     var formValues = Array.from(enableInputs).reduce(function(values, input){
-        
-                        console.log((values[input.name] = input.value));
-                        return (values[input.name] = input.value) && values;
+                        switch(input.type){
+                            case 'radio':
+                                values[input.name] = formElement.querySelector('input[name="'+ input.name +'"]:checked').value;
+                                break;
+                            case 'checkbox':
+                                if(!input.matches(':checked')) 
+                                {
+                                    values[input.name] = [];
+                                    return values;
+                                }
+                                if(!Array.isArray(values[input.name])){
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
+                                break;
+                            default:
+                                values[input.name] = input.value;
+                        }
+                        return  values;
                     }, {});
 
                     options.onSubmit(formValues);
@@ -75,21 +116,24 @@ function Validator(options){
                 selectorRules[rule.selector] = [rule.test];
             }
 
-            var inputElement = formElement.querySelector(rule.selector);
+            var inputElements = formElement.querySelectorAll(rule.selector);
 
-            if(inputElement){
-                inputElement.onblur = function()
-                {
-                    validate(inputElement, rule);
+            Array.from(inputElements).forEach(function(inputElement){
+                if(inputElement){
+                    inputElement.onblur = function()
+                    {
+                        validate(inputElement, rule);
+                    }
+    
+                    //Xử lý khi người dùng nhập vào input
+                    inputElement.oninput = function(){
+                        var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
+                        errorElement.innerText = '';
+                        getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+                    }
                 }
+            });
 
-                //Xử lý khi người dùng nhập vào input
-                inputElement.oninput = function(){
-                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
-                    errorElement.innerText = '';
-                    inputElement.parentElement.classList.remove('invalid');
-                }
-            }
         });
 
         // console.log(selectorRules);
@@ -100,7 +144,7 @@ Validator.isRequired = function (selector, message){
     return {
         selector: selector,
         test: function (value){
-            return value.trim() ? undefined : message || 'Vui lòng nhập trường này';
+            return value ? undefined : message || 'Vui lòng nhập trường này';
         }
     };
 }
